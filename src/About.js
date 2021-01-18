7537aa097eb5a5fbe8d31c267a67e00372782c06
mini pony <northjw1@gmail.com>
2021-01-17 21:52:31 -0500

    adding projects, articles, about

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function NewComponent(props) {
+  return (
+    <div>
+      <h1>About Josh North</h1>
+    </div>
+  )
+}
+ 
+export default NewComponent;
---
+++ src/Articles.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function NewComponent(props) {
+  return (
+    <div>
+      <h1>Articles</h1>
+    </div>
+  )
+}
+ 
+export default NewComponent;
